da7ab28b08e77f189e71d201ecdf06ad4c76337b
Adrian Stasiv <adrian.stasiv@otakoyi.com>
2023-06-20 17:45:55 +0300

    first commit

---
+++ config/database.js
@@ -0,0 +1,18 @@
+const path = require("path");
+
+module.exports = ({ env }) => ({
+  connection: {
+    client: "sqlite",
+    connection: {
+      filename: path.join(
+        __dirname,
+        "..",
+        env("DATABASE_FILENAME", ".tmp/data.db")
+      ),
+    },
+    useNullAsDefault: true,
+  },
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
